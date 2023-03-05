bd69732b190c39ccb650a6b57f2e82fe53ee3422
Dev-scott <sadoscott25@gmail.com>
2023-03-06 00:42:30 +0100

    ajout de la page d'accueil

--- App.js
+++ App.js
@@ -80,40 +80,9 @@ const App = () => {
         <Stack.Screen
           name="Home"
           component={Tabs}
-          options={{
-            title: null,
-            headerStyle: {
-              backgroundColor: COLORS.white
-            },
-            headerLeft: ({ onPress }) =>
-              <TouchableOpacity
-                style={{ marginLeft: SIZES.padding }}
-                onPress={onPress}
-              >
-                <Image
-                  source={icons.back}
-                  resizeMode="contain"
-                  style={{
-                    width: 25,
-                    height: 25
-                  }}
-                />
-              </TouchableOpacity>,
-            headerRight: () =>
-              <TouchableOpacity
-                style={{ marginRight: SIZES.padding }}
-                onPress={() => console.log("Menu")}
-              >
-                <Image
-                  source={icons.menu}
-                  resizeMode="contain"
-                  style={{
-                    width: 25,
-                    height: 25
-                  }}
-                />
-              </TouchableOpacity>
-          }}
+          options={{ headerShown: false }}
+
+       
         />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/Home.js
+++ screens/Home.js
@@ -1,36 +1,203 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  TouchableOpacity,
-  FlatList
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import TopBar from "../components/TopBar";
 
-import { images, icons, COLORS, FONTS, SIZES } from "../constants";
-
-const Home = ({ navigation }) => {
-  return <View style={styles.container} />;
-};
+export default function Home() {
+  return (
+    <View style={styles.mainPage}>
+      <TopBar />
+      <View style={styles.previewAreaL}>
+        <Text style={styles.title}>Catégorie de vêtement</Text>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          style={styles.preview}
+        >
+          <Image
+            source={require("../assets/images/linges/linge1.png")}
+            style={styles.myImages}
+          />
+          <Image
+            source={require("../assets/images/linges/linge2.png")}
+            style={styles.myImages}
+          />
+          <Image
+            source={require("../assets/images/linges/linge3.png")}
+            style={styles.myImages}
+          />
+          <Image
+            source={require("../assets/images/linges/linge4.png")}
+            style={styles.myImages}
+          />
+          <Image
+            source={require("../assets/images/linges/linge1.png")}
+            style={styles.myImages}
+          />
+        </ScrollView>
+      </View>
+      <View style={styles.previewAreaS}>
+        <Text style={styles.title}>Nos services</Text>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          style={styles.preview}
+        >
+          <View style={styles.serviceSpec}>
+            <Image
+              source={require("../assets/images/services/nettoyageSec.jpg")}
+              style={styles.myServices}
+            />
+            <Text style={styles.name}>Lavage à sec</Text>
+            <Text style={styles.nameSpec}>
+              service de lavage à sec sans mousse
+            </Text>
+          </View>
+          <View style={styles.serviceSpec}>
+            <Image
+              source={require("../assets/images/services/nettoyageSec.jpg")}
+              style={styles.myServices}
+            />
+            <Text style={styles.name}>Lavage à sec</Text>
+            <Text style={styles.nameSpec}>
+              service de lavage à sec sans mousse
+            </Text>
+          </View>
+          <View style={styles.serviceSpec}>
+            <Image
+              source={require("../assets/images/services/nettoyageSec.jpg")}
+              style={styles.myImages}
+            />
+            <Text style={styles.name}>Lavage à sec</Text>
+            <Text style={styles.nameSpec}>
+              service de lavage à sec sans mousse
+            </Text>
+          </View>
+          <View style={styles.serviceSpec}>
+            <Image
+              source={require("../assets/images/services/nettoyageSec.jpg")}
+              style={styles.myServices}
+            />
+            <Text style={styles.name}>Lavage à sec</Text>
+            <Text style={styles.nameSpec}>
+              service de lavage à sec sans mousse
+            </Text>
+          </View>
+          <View style={styles.serviceSpec}>
+            <Image
+              source={require("../assets/images/services/nettoyageSec.jpg")}
+              style={styles.myServices}
+            />
+            <Text style={styles.name}>Lavage à sec</Text>
+            <Text style={styles.nameSpec}>
+              service de lavage à sec sans mousse
+            </Text>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.previewAreaP}>
+        <Text style={styles.title}>Les pressing près de chez vous</Text>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          style={styles.preview}
+        >
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+          <Image
+            source={require("../assets/images/services/pressing.png")}
+            style={styles.myPressing}
+          />
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.white
+  mainPage: {},
+  previewAreaL: {
+    justifyContent: "space-between",
+    marginLeft: 16
   },
-  shadow: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5
+  myImages: {
+    marginRight: 12,
+    width: 82,
+    height: 116
+  },
+  title: {
+    paddingVertical: 16,
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  previewAreaS: {
+    paddingTop: 32,
+    marginLeft: 16
+  },
+  serviceSpec: {
+    height: 230,
+    width: 225,
+    marginRight: 16,
+    justifyContent: "space-between"
+  },
+  myServices: {
+    height: 167,
+    width: 207
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  nameSpec: {
+    fontSize: 12,
+    color: "#8E9090",
+    width: 150
+  },
+  previewAreaP: {
+    marginLeft: 16,
+    marginTop: 24
+  },
+  myPressing: {
+    height: 50,
+    width: 65,
+    marginRight: 12,
+    borderRadius: 30
   }
 });
-
-export default Home;
