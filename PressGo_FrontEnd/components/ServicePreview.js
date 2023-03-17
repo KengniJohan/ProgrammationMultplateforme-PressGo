19d996239f60d4bd375e78286aa06599ec843d0a
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-03-17 10:37:23 +0100

    Création du composant ServicePreview

---
+++ PressGo_FrontEnd/components/ServicePreview.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, View, Image, Text, Pressable } from "react-native";
+
+const ServicePreview = ({imageSource, title, description, navigation}) => {
+    return(
+        <View style={styles.serviceSpec}>
+            <Pressable 
+                onPress={
+                    () => navigation.navigate({name : 'Bookmark'})
+                }
+            >
+                <Image
+                    source={imageSource}
+                    style={styles.myServices}
+                />
+                <Text style={styles.name}>{title}</Text>
+                <Text style={styles.nameSpec}>{description}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+    serviceSpec: {
+        height: 230,
+        width: 225,
+        marginRight: 16,
+        justifyContent: "space-between"
+    },
+    name: {
+      fontSize: 16,
+      fontWeight: "bold"
+    },
+    nameSpec: {
+      fontSize: 12,
+      color: "#8E9090",
+      width: 150
+    },
+    myServices: {
+      height: 167,
+      width: 207
+    }
+})
+
+export default ServicePreview
